bdb50bc1ae59356b733d3e74f258d7d55cf23517
Amoong <moo1323@naver.com>
2023-04-05 19:46:05 +0900

    advanced template and slot

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-customElements.define("my-paragraph", class extends HTMLElement {
+customElements.define("element-details", class extends HTMLElement {
   constructor() {
     super()
 
-    const template = document.getElementById("my-paragraph")
-    const templateContent = template.content
-
+    const template = document.getElementById("element-details-template").content
     const shadowRoot = this.attachShadow({ mode: "open" })
-    shadowRoot.appendChild(templateContent.cloneNode(true))
+    shadowRoot.appendChild(template.cloneNode(true))
   }
 })
