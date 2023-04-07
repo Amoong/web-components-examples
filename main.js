974e79c72adca5f78816641b659cce7825be0de1
Amoong <moo1323@naver.com>
2023-04-07 17:53:11 +0900

    Dev setting

--- main.js
+++ main.js
@@ -1,90 +1,57 @@
-customElements.define(
-  "custom-square",
-  class extends HTMLElement {
-    constructor() {
-      super();
+const html = String.raw;
 
-      const shadow = this.attachShadow({ mode: "open" });
+const template = document.createElement("template");
 
-      const div = document.createElement("div");
-      const style = document.createElement("style");
-      shadow.appendChild(style);
-      shadow.appendChild(div);
+// html: For prettier auto formatting
+template.innerHTML = html`<style>
+    * {
+      font-size: 200%;
     }
 
-    connectedCallback() {
-      console.log("Custom square element added to page.");
-      updateStyle(this);
+    span {
+      width: 4rem;
+      display: inline-block;
+      text-align: center;
     }
 
-    disconnectedCallback() {
-      console.log("Custom square element removed from page.");
+    button {
+      width: 4rem;
+      height: 4rem;
+      border: none;
+      border-radius: 10px;
+      background-color: seagreen;
+      color: white;
     }
+  </style>
+  <button id="dec">-</button>
+  <span id="count"></span>
+  <button id="inc">+</button>`;
 
-    adoptedCallback() {
-      console.log("Custom square element moved to new page.");
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      console.log("Custom square element attributes changed.");
-      updateStyle(this);
-    }
-
-    static get observedAttributes() {
-      return ["c", "l"];
-    }
+class MyCounter extends HTMLElement {
+  constructor() {
+    super();
+    this.count = 0;
+    this.attachShadow({ mode: "open" });
   }
-);
 
-function updateStyle(elem) {
-  const shadow = elem.shadowRoot;
-  shadow.querySelector("style").textContent = `
-    div {
-      width: ${elem.getAttribute("l")}px;
-      height: ${elem.getAttribute("l")}px;
-      background-color: ${elem.getAttribute("c")}
-    }
-  `;
+  connectedCallback() {
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.getElementById("inc").onclick = () => this.inc();
+    this.shadowRoot.getElementById("dec").onclick = () => this.dec();
+    this.update(this.count);
+  }
+
+  inc() {
+    this.update(++this.count);
+  }
+
+  dec() {
+    this.update(--this.count);
+  }
+
+  update(count) {
+    this.shadowRoot.getElementById("count").innerHTML = count;
+  }
 }
 
-const add = document.querySelector(".add");
-const update = document.querySelector(".update");
-const remove = document.querySelector(".remove");
-let square;
-
-update.disabled = true;
-remove.disabled = true;
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-add.onclick = function () {
-  // Create a custom square element
-  square = document.createElement("custom-square");
-  square.setAttribute("l", "100");
-  square.setAttribute("c", "red");
-  document.body.appendChild(square);
-
-  update.disabled = false;
-  remove.disabled = false;
-  add.disabled = true;
-};
-
-update.onclick = function () {
-  // Randomly update square's attributes
-  square.setAttribute("l", random(50, 200));
-  square.setAttribute(
-    "c",
-    `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
-  );
-};
-
-remove.onclick = function () {
-  // Remove the square
-  document.body.removeChild(square);
-
-  update.disabled = true;
-  remove.disabled = true;
-  add.disabled = false;
-};
+customElements.define("my-counter", MyCounter);
